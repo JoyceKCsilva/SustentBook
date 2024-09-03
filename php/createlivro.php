575406624243087291701459092590900549ca8c
Alyson-Vinicius <149380852+Alyson-Vinicius@users.noreply.github.com>
2024-09-03 14:54:35 -0300

    atualização teste CSS

--- php/createlivro.php
+++ php/createlivro.php
@@ -68,13 +68,57 @@ if (isset($_SESSION['USR_ID'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar livro</title>
-    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/loginstyle.css">
 </head>
 <body>
+
+<?php if (isset($_SESSION['USR_EMAIL'])): ?>
+<div class="wrapper">
+        <aside id="sidebar">     
+            <div class="data-bs-target">
+                <button class="toggle-btn" type="menu">
+                    <img src="../img/logo.svg" alt="logo">
+                </button>
+                    <ul class="sidebar-nav">
+                        <li class="sidebar-item">
+                            <a href="index.php" class="sidebar-link">
+                                <i class="bi bi-house"></i>
+                                <span>Perfil</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="php/meuperfil.php" class="sidebar-link">
+                                <i class="lni lni-user"></i>
+                                <span>Perfil</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="php/buscar.php" class="sidebar-link">
+                                <i class="lni lni-search-alt"></i>
+                                <span>Procurar</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="php/meuslivros.php" class="sidebar-link">
+                                <i class="lni lni-book"></i>
+                            <span>Meus Livros</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="php/listadesejos.php" class="sidebar-link">
+                                <i class="lni lni-list"></i>
+                                <span>Lista de Desejos</span>
+                            </a>
+                        </li>
+
+                        <div class="sidebar-footer">
+                        <a href="php/logout.php" class="sidebar-link">
+                            <i class="lni lni-exit"></i>
+                            <span>Sair</span>
+                        </a>
+                    </u>
+                </div>
+                <?php endif; ?>
 
     <div class="container">
             <img src="../../img/logo.svg">
